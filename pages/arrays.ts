022a325b74ba7c56031cbfa20eaf8a776cb90afa
UROBBYU <srobby228@gmail.com>
2022-04-07 18:03:22 +0300

    Upgraded lab4

--- pages/arrays.ts
+++ pages/arrays.ts
@@ -1,8 +1,11 @@
 import express from 'express'
+import fs from 'fs'
+import php from 'http-php'
 
 module.exports = express.Router()
 
-.get('/', (req, res, next) => {
-    require('http-php')('pages/arrays/index.php')(req, res)
-    .catch(next)
-})
+.get('/', (req, res, next) => fs.createReadStream('pages/arrays/index.html').pipe(res.type('html')))
+
+.all('/php', async (req, res, next) => php('pages/arrays/index.php')(req, res).catch(next))
+
+.use('/st/', express.static('pages/arrays/static'))
--- pages/arrays/index.php
+++ pages/arrays/index.php
@@ -1,71 +1,38 @@
 <?php
-    // Array of students
-    $group = array();
-
-    // Parse url params for array of students
-    $arr = $_GET['array'];
-    if ($arr) {
-        $arr = explode('|', $arr);
-        foreach ($arr as $val) {
-            $parsed = explode(',', $val);
-            $group[] = array(
-                'sur' => $parsed[0],
-                'rat' => $parsed[1],
-                'sex' => $parsed[2]
-            );
-        }
-    }
+    // Read json data
+    $body = json_decode(file_get_contents('php://input'), true);
+    $group = $body['array'];
 
     // Count male/female
     $counter_m = 0;
     $counter_f = 0;
     foreach ($group as $student) {
-        if ($student['sex'] == 'M') $counter_m++;
+        if ($student['sex'] == 'Ч') $counter_m++;
         else $counter_f++;
     }
 
-    // Find student by surname
-    function find_student($sur) {
+    // Find student by parameters
+    function find_student($sur, $rate = false, $sex = false) {
         global $group;
-        foreach ($group as $student) {
-            if ($student['sur'] == $sur) return $student;
+        foreach ($group as $i => $student) {
+            if (($sur == ($student['sur'] == $sur)) && ($rate == ($student['rate'] == $rate)) && ($sex == ($student['sex'] == $sex))) return $i + 1;
         }
     }
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Lab 4 | Arrays</title>
-</head>
-<body>
-    <textarea rows="4"></textarea><br/>
-    <button>Зберегти</button>
-    <p>Хлопців: <?php echo($counter_m); ?></p>
-    <p>Дівчат: <?php echo($counter_f); ?></p>
-    <?php
-        // Check if find is specified
-        if ($_GET['find']) {
-            // Find student
-            $found = find_student($_GET['find']);
 
-            // If found, output
-            if ($found)
-                echo '<p>Знайдено: ', $found['sur'], ' | ', $found['rat'], ' | ', ($found['sex'] == 'M' ? 'Хлопець' : 'Дівчина'), '</p>';
-            else
-                echo '<p>Студента з прізвищем \'', $_GET['find'], '\' не знайдено</p>';
+    // Execution
+    header('Content-Type: application/json');
+    if ($body['type'] == 'find') {
+        if (!($body['sur'] || $body['rate'] || $body['sex'])) echo '{}';
+        else {
+            $found = find_student($body['sur'], $body['rate'], $body['sex']);
+            if ($found) echo json_encode(array('id' => $found));
+            else echo '{}';
         }
-    ?>
-    <a href="?array=Kirigaya%2C3.1%2CM|Yuuki%2C4.8%2CF|Asada%2C4.5%2CF|Tsuboi%2C3.6%2CM|Ayano%2C3.9%2CF&find=Yuuki">Заготовка</a>
-    <script>
-        const arrs = document.querySelector('textarea')
-        const sub = document.querySelector('button')
-
-        arrs.value = '<?php if ($arr) echo(join('\\n', $arr)); ?>'
-
-        sub.addEventListener('click', () => {
-            location = 'https://urepo.online/arrays?array=' + encodeURIComponent(arrs.value.replaceAll('\n', '|'))
-        })
-    </script>
-</body>
-</html>
+    } else {
+        echo json_encode(array(
+            'array' => $group,
+            'boys' => $counter_m,
+            'girls' => $counter_f
+        ));
+    }
+?>
