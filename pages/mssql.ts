bd9cbf12fb3e26ff17368c79238399eb92cd5f3b
UROBBYU <srobby228@gmail.com>
2022-04-07 18:03:06 +0300

    Rewrote routes

--- pages/mssql.ts
+++ pages/mssql.ts
@@ -4,10 +4,7 @@ import php from 'http-php'
 
 module.exports = express.Router()
 
-.get('/', (req, res, next) => {
-    res.type('html')
-    fs.createReadStream('pages/mssql/index.html').pipe(res)
-})
+.get('/', (req, res, next) => fs.createReadStream('pages/mssql/index.html').pipe(res.type('html')))
 
 .all('/php', async (req, res, next) => php('pages/mssql/index.php')(req, res).catch(next))
 
